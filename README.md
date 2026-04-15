45968c1fe3bf27f6348fb8d81eded9d7245d1b8f
CiReal <Kydan301193@gmail.com>
2026-04-15 20:11:47 +0700

    Add to cart

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ The React Compiler is not enabled on this template because of its impact on dev 
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+giải thích bản chất cardItems thay đổi
+dù navbar hay bất cứ đâu,khi gọi addToCart thực chất là gọi hà trong ShopContext.jsx
+hàm addToCart sẽ thực hiện setCardItems(...) dùng useState
+vì cartItems là một useState nằm trong ShopContext nên khi thay đổi toàn bộ vùng Context đó bị đánh 
+dấu cần phải render lại
+--Tổng kết lại thì getCartCount ăn theo cartItems(useState) ,navbar sử dụng getCartCount để render đơn hàng dù ko thấy nó sử dụng useState nhưng nó vấn tự re render khi cartItems thay đổi 
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,9 +10,50 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});//cartItems là object lồng nhau nên có thể for
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);//Deep copy
+    if (!size) {
+      toast.error("Select Product Size");
+      return;
+    }
+    if (cartData[itemId]) { //ví dụ cartData["001"] (id)
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      }
+      else {
+        cartData[itemId][size] = 1;
+      }
+
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  }
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+
+        }
+      }
+    }
+
+    return totalCount;
+  }
+
+
   const value = {
     products, currency, delivery_fee,
-    search, setSearch, showSearch, setShowSearch
+    search, setSearch, showSearch, setShowSearch,
+    cartItems, addToCart, getCartCount
   }
 
   return (
@@ -22,3 +64,17 @@ const ShopContextProvider = (props) => {
 }
 
 export default ShopContextProvider;
+
+// {
+//   "product_id_001": {
+//     "S": 2,
+//     "M": 1,
+//     "XL": 5
+//   },
+//   "product_id_002": {
+//     "L": 1
+//   },
+//   "product_id_003": {
+//     "XL": 3
+//   }
+// }
