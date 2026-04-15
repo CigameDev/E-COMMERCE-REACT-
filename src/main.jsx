2e06086712952640abd09635f29dfe5d71124ce1
CiReal <Kydan301193@gmail.com>
2026-04-15 23:04:06 +0700

    Update Order

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import ShopContextProvider from './context/ShopContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ShopContextProvider >
-      <BrowserRouter>
+    <BrowserRouter>
+      <ShopContextProvider >
         <App />
-      </BrowserRouter>,
-    </ShopContextProvider>
+      </ShopContextProvider>
+    </BrowserRouter>,
   </StrictMode>
 
 )
